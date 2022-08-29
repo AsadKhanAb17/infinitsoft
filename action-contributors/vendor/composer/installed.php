78207636a673b120c9c9be472f03b69407edb345
AsadKhanAb17 <asadkhan10222@gmail.com>
2022-08-29 10:59:27 +0500

    first commit

---
+++ action-contributors/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'minicli/minicli' => array(
+            'pretty_version' => '3.2.0',
+            'version' => '3.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../minicli/minicli',
+            'aliases' => array(),
+            'reference' => '839c9c95d89a40d1793f75e7624e7edf0ab889da',
+            'dev_requirement' => false,
+        ),
+    ),
+);
